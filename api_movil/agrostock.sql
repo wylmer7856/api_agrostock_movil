4d6a3bc5d45d0eca34f2902d8087cfe476800ffa
AndresSaavedra2952 <189684621+AndresSaavedra2952@users.noreply.github.com>
2025-09-12 22:42:47 -0500

    se agrego la nueva base de datos

---
+++ api_movil/agrostock.sql
@@ -0,0 +1,479 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-09-2025 a las 05:33:49
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `agrostock`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alertas_stock`
+--
+
+CREATE TABLE `alertas_stock` (
+  `id_alerta` int(11) NOT NULL,
+  `id_producto` int(11) DEFAULT NULL,
+  `stock_actual` int(11) DEFAULT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp(),
+  `mensaje` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ciudades`
+--
+
+CREATE TABLE `ciudades` (
+  `id_ciudad` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `id_departamento` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `ciudades`
+--
+
+INSERT INTO `ciudades` (`id_ciudad`, `nombre`, `id_departamento`) VALUES
+(1, 'Leticia', 27),
+(2, 'Medellín', 5),
+(3, 'Arauca', 23),
+(4, 'Barranquilla', 11),
+(5, 'Cartagena', 12),
+(6, 'Tunja', 2),
+(7, 'Manizales', 6),
+(8, 'Florencia', 28),
+(9, 'Yopal', 24),
+(10, 'Popayán', 20),
+(11, 'Valledupar', 17),
+(12, 'Quibdó', 22),
+(13, 'Montería', 13),
+(14, 'Bogotá D.C.', 1),
+(15, 'Inírida', 29),
+(16, 'San José del Guaviare', 30),
+(17, 'Neiva', 4),
+(18, 'Riohacha', 16),
+(19, 'Santa Marta', 15),
+(20, 'Villavicencio', 25),
+(21, 'Pasto', 21),
+(22, 'Cúcuta', 10),
+(23, 'Mocoa', 31),
+(24, 'Armenia', 8),
+(25, 'Pereira', 7),
+(26, 'Bucaramanga', 9),
+(27, 'Sincelejo', 14),
+(28, 'Ibagué', 3),
+(29, 'Cali', 19),
+(30, 'Mitú', 32),
+(31, 'Puerto Carreño', 26),
+(32, 'San Andrés', 33);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `consejos`
+--
+
+CREATE TABLE `consejos` (
+  `id_consejo` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `contenido` text NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `departamentos`
+--
+
+CREATE TABLE `departamentos` (
+  `id_departamento` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `id_region` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `departamentos`
+--
+
+INSERT INTO `departamentos` (`id_departamento`, `nombre`, `id_region`) VALUES
+(1, 'Cundinamarca', 1),
+(2, 'Boyacá', 1),
+(3, 'Tolima', 1),
+(4, 'Huila', 1),
+(5, 'Antioquia', 1),
+(6, 'Caldas', 1),
+(7, 'Risaralda', 1),
+(8, 'Quindío', 1),
+(9, 'Santander', 1),
+(10, 'Norte de Santander', 1),
+(11, 'Atlántico', 2),
+(12, 'Bolívar', 2),
+(13, 'Córdoba', 2),
+(14, 'Sucre', 2),
+(15, 'Magdalena', 2),
+(16, 'La Guajira', 2),
+(17, 'Cesar', 2),
+(18, 'San Andrés y Providencia', 2),
+(19, 'Valle del Cauca', 3),
+(20, 'Cauca', 3),
+(21, 'Nariño', 3),
+(22, 'Chocó', 3),
+(23, 'Arauca', 4),
+(24, 'Casanare', 4),
+(25, 'Meta', 4),
+(26, 'Vichada', 4),
+(27, 'Amazonas', 5),
+(28, 'Caquetá', 5),
+(29, 'Guainía', 5),
+(30, 'Guaviare', 5),
+(31, 'Putumayo', 5),
+(32, 'Vaupés', 5),
+(33, 'Archipiélago de San Andrés, Providencia y Santa Catalina', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_pedidos`
+--
+
+CREATE TABLE `detalle_pedidos` (
+  `id_detalle` int(11) NOT NULL,
+  `id_pedido` int(11) DEFAULT NULL,
+  `id_producto` int(11) DEFAULT NULL,
+  `precio_unitario` decimal(10,2) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `Precio_total` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `id_pedido` int(11) NOT NULL,
+  `id_consumidor` int(11) DEFAULT NULL,
+  `id_productor` int(11) DEFAULT NULL,
+  `fecha` date NOT NULL,
+  `estado` enum('pendiente','confirmado','comprado') DEFAULT 'pendiente',
+  `total` decimal(10,2) NOT NULL,
+  `direccionEntrega` text NOT NULL,
+  `notas` text NOT NULL,
+  `fecha_entrega_estimada` date NOT NULL,
+  `metodo_pago` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id_producto` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `stockMinimo` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `id_ciudad_origen` int(11) DEFAULT NULL,
+  `unidadMedida` varchar(20) NOT NULL,
+  `pesoAprox` decimal(8,2) NOT NULL,
+  `imagenPrincipal` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `regiones`
+--
+
+CREATE TABLE `regiones` (
+  `id_region` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `regiones`
+--
+
+INSERT INTO `regiones` (`id_region`, `nombre`) VALUES
+(1, 'Andina'),
+(2, 'Caribe'),
+(3, 'Pacífica'),
+(4, 'Orinoquía'),
+(5, 'Amazonía '),
+(6, 'Insular');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `resenas`
+--
+
+CREATE TABLE `resenas` (
+  `id_resena` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `id_producto` int(11) DEFAULT NULL,
+  `id_pedido` int(11) NOT NULL,
+  `comentario` text DEFAULT NULL,
+  `calificacion` int(11) DEFAULT NULL CHECK (`calificacion` between 1 and 5),
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `id_ciudad` int(11) DEFAULT NULL,
+  `rol` enum('admin','consumidor','productor') NOT NULL DEFAULT 'consumidor'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nombre`, `email`, `password`, `telefono`, `direccion`, `id_ciudad`, `rol`) VALUES
+(1, 'wilmer', 'wilmer@mail.com', '12345678', '3222212121', 'cl10#4', 14, 'consumidor');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alertas_stock`
+--
+ALTER TABLE `alertas_stock`
+  ADD PRIMARY KEY (`id_alerta`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `ciudades`
+--
+ALTER TABLE `ciudades`
+  ADD PRIMARY KEY (`id_ciudad`),
+  ADD KEY `id_departamento` (`id_departamento`);
+
+--
+-- Indices de la tabla `consejos`
+--
+ALTER TABLE `consejos`
+  ADD PRIMARY KEY (`id_consejo`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `departamentos`
+--
+ALTER TABLE `departamentos`
+  ADD PRIMARY KEY (`id_departamento`),
+  ADD KEY `id_region` (`id_region`);
+
+--
+-- Indices de la tabla `detalle_pedidos`
+--
+ALTER TABLE `detalle_pedidos`
+  ADD PRIMARY KEY (`id_detalle`),
+  ADD KEY `id_pedido` (`id_pedido`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id_pedido`),
+  ADD KEY `id_consumidor` (`id_consumidor`),
+  ADD KEY `id_productor` (`id_productor`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_ciudad_origen` (`id_ciudad_origen`);
+
+--
+-- Indices de la tabla `regiones`
+--
+ALTER TABLE `regiones`
+  ADD PRIMARY KEY (`id_region`);
+
+--
+-- Indices de la tabla `resenas`
+--
+ALTER TABLE `resenas`
+  ADD PRIMARY KEY (`id_resena`),
+  ADD UNIQUE KEY `id_pedido` (`id_pedido`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `id_ciudad` (`id_ciudad`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alertas_stock`
+--
+ALTER TABLE `alertas_stock`
+  MODIFY `id_alerta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ciudades`
+--
+ALTER TABLE `ciudades`
+  MODIFY `id_ciudad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT de la tabla `consejos`
+--
+ALTER TABLE `consejos`
+  MODIFY `id_consejo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `departamentos`
+--
+ALTER TABLE `departamentos`
+  MODIFY `id_departamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_pedidos`
+--
+ALTER TABLE `detalle_pedidos`
+  MODIFY `id_detalle` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `regiones`
+--
+ALTER TABLE `regiones`
+  MODIFY `id_region` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `resenas`
+--
+ALTER TABLE `resenas`
+  MODIFY `id_resena` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alertas_stock`
+--
+ALTER TABLE `alertas_stock`
+  ADD CONSTRAINT `alertas_stock_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`);
+
+--
+-- Filtros para la tabla `ciudades`
+--
+ALTER TABLE `ciudades`
+  ADD CONSTRAINT `ciudades_ibfk_1` FOREIGN KEY (`id_departamento`) REFERENCES `departamentos` (`id_departamento`);
+
+--
+-- Filtros para la tabla `consejos`
+--
+ALTER TABLE `consejos`
+  ADD CONSTRAINT `consejos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Filtros para la tabla `departamentos`
+--
+ALTER TABLE `departamentos`
+  ADD CONSTRAINT `departamentos_ibfk_1` FOREIGN KEY (`id_region`) REFERENCES `regiones` (`id_region`);
+
+--
+-- Filtros para la tabla `detalle_pedidos`
+--
+ALTER TABLE `detalle_pedidos`
+  ADD CONSTRAINT `detalle_pedidos_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id_pedido`),
+  ADD CONSTRAINT `detalle_pedidos_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`);
+
+--
+-- Filtros para la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`id_consumidor`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `pedidos_ibfk_2` FOREIGN KEY (`id_productor`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `productos_ibfk_2` FOREIGN KEY (`id_ciudad_origen`) REFERENCES `ciudades` (`id_ciudad`);
+
+--
+-- Filtros para la tabla `resenas`
+--
+ALTER TABLE `resenas`
+  ADD CONSTRAINT `resenas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `resenas_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`),
+  ADD CONSTRAINT `resenas_ibfk_3` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id_pedido`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_ciudad`) REFERENCES `ciudades` (`id_ciudad`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
